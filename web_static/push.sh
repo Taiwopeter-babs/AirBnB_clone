7b33673ff802391e89d7eef4c72c877240d24cf7
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-19 23:06:20 +0100

    add responsive design

--- web_static/push.sh
+++ web_static/push.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Automates basic git actions
 
+# Second argument on command line
 arg1="$1"
+# Third argument on command line
 arg2="$2"
 
-git add $arg1
+git add "$arg1"
 
 git commit -m "$arg2"
+
 
 git push
